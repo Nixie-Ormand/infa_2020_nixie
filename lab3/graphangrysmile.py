35a77913cf4f038b6a5b4a4eddeb8d073c173f7a
Nixie <59831223+Nixie-Ormand@users.noreply.github.com>
2020-02-10 17:47:39 +0300

    Add graphangryface.ry

---
+++ lab3/graphangrysmile.py
@@ -0,0 +1,31 @@
+from graph import *
+
+brushColor("yellow")
+circle(200, 200, 100)
+
+penColor("black")
+penSize(2)
+brushColor("red")
+circle(160, 170, 26)
+penColor("black")
+penSize(2)
+brushColor("red")
+circle(250, 168, 22)
+
+brushColor("black")
+circle(160, 170, 15)
+brushColor("black")
+circle(250, 168, 14)
+
+brushColor("Black")
+rectangle(165, 250, 250, 267)
+brushColor("Black")
+polygon([(110, 100),
+         (190, 150),
+         (180,153),
+         (105,106)])
+polygon([(220, 150),
+         (290, 130),
+         (295,135),
+         (215,158)])
+run()
